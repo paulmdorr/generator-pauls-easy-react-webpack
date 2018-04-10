be3c6b783658c91d8c6e31f323a4847e1e1ea5af
Paul M Dorr <paul.mdorr@gmail.com>
2018-04-10 17:12:17 -0300

    Change import CSS file name (fix #6)

--- generators/app/templates/final/src/scenes/App/App.jsx
+++ generators/app/templates/final/src/scenes/App/App.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { render } from 'react-dom'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import injectTapEventPlugin from 'react-tap-event-plugin'
-import style from './style.css'
+import style from './styles.css'
 
 injectTapEventPlugin()
 
